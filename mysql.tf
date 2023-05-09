35b732155293ed2c2f2155d98df56b3eb66aa0f2
zelleg <massyl.zelleg2201@gmail.com>
2023-05-09 14:37:59 +0200

    Update: network config mysql module

--- mysql.tf
+++ mysql.tf
@@ -10,16 +10,19 @@ resource "google_sql_database_instance" "cloud_sql_instance" {
       enabled = true
     }
 
-    ip_configuration {
-      ipv4_enabled    = true
-      private_network = google_compute_network.vpc.self_link
+    network    = google_compute_network.vpc.name
+    subnetwork = google_compute_subnetwork.subnet.name
 
-      # Configure the private IP address of the Cloud SQL instance to use
-      # a specific IP address within the subnet range.
-      # This assumes that the subnet range is 10.10.0.0/24 and the IP address
-      # we want to use for the Cloud SQL instance is 10.10.0.2.
-      private_network_ip = "10.10.0.2"
-    }
+    # ip_configuration {
+    #   ipv4_enabled    = true
+    #   private_network = google_compute_network.vpc.self_link
+
+    #   # Configure the private IP address of the Cloud SQL instance to use
+    #   # a specific IP address within the subnet range.
+    #   # This assumes that the subnet range is 10.10.0.0/24 and the IP address
+    #   # we want to use for the Cloud SQL instance is 10.10.0.2.
+    #   private_network_ip = "10.10.0.2"
+    # }
   }
 }
 
@@ -30,14 +33,14 @@ resource "google_sql_database" "database" {
   collation  = "utf8_general_ci"
 }
 
-resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
-  auto_create_subnetworks = false
-}
+# resource "google_compute_network" "vpc" {
+#   name                    = "${var.project_id}-vpc"
+#   auto_create_subnetworks = false
+# }
 
-resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
-  region        = var.region
-  network       = google_compute_network.vpc.self_link
-  ip_cidr_range = "10.10.0.0/24"
-}
+# resource "google_compute_subnetwork" "subnet" {
+#   name          = "${var.project_id}-subnet"
+#   region        = var.region
+#   network       = google_compute_network.vpc.self_link
+#   ip_cidr_range = "10.10.0.0/24"
+# }
