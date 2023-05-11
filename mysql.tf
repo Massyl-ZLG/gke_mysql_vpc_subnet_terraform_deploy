2d7b01c2807e1e27e3ec5760c94a38f1e7a2abf6
massylz <massyl.zelleg2201@gmail.com>
2023-05-11 11:29:26 +0200

    Fix: mysql-instance:SERVICE_NETWORKING_NOT_ENABLED error

--- mysql.tf
+++ mysql.tf
@@ -5,14 +5,17 @@ resource "google_sql_database_instance" "cloud_sql_instance" {
 
   settings {
     tier = var.instance_type
-    authorized_networks {
-      name = "default"
-    }
     ip_configuration {
-      ipv4_enabled                                  = false
-      private_network                               = google_compute_network.vpc.id
+      ipv4_enabled    = false
+      private_network = google_compute_network.vpc.id
+      
+      authorized_networks {
+        name = "default"
+      }
+      
       #enable_private_path_for_google_cloud_services = true
     }
+    
     backup_configuration {
       enabled = true
     }
@@ -26,5 +29,3 @@ resource "google_sql_database" "database" {
   charset    = "utf8"
   collation  = "utf8_general_ci"
 }
-
-
